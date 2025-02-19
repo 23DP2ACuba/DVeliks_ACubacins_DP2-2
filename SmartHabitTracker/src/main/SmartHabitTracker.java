73fc6148811411e5ec41feb947b0db309dd83770
ArtjomsCubacins <166109704+23DP2ACuba@users.noreply.github.com>
2025-02-19 12:18:49 +0200

    Add files via upload
    structue

---
+++ SmartHabitTracker/src/main/SmartHabitTracker.java
@@ -0,0 +1,12 @@
+package main;
+public class SmartHabitTracker {
+    public static void main(String[] args) {
+        System.out.println("Hello, Smart Habit Tracker!");
+        System.out.println("Hello, Smart Habit Tracker!");
+        System.out.println("Hello, Smart Habit Tracker!");
+        System.out.println("Hello, Smart Habit Tracker!");
+        System.out.println("Hello, Smart Habit Tracker!");
+        System.out.println("Hello, Smart Habit Tracker!");
+        System.out.println("Hello, Smart Habit Tracker!");
+    }
+}
