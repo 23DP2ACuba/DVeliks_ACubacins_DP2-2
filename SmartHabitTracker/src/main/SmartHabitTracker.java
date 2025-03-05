ff6de5ce092dc503b5c72b50d6f7aa4abe9e555a
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2025-03-05 13:16:16 +0200

    Delete SmartHabitTracker directory

--- SmartHabitTracker/src/main/SmartHabitTracker.java
+++
@@ -1,9 +0,0 @@
-package main;
-import main.ui.ConsoleUI;
-
-public class SmartHabitTracker {
-    public static void main(String[] args) {
-        ConsoleUI console = new ConsoleUI();
-        console.showMenu();
-    }
-}
